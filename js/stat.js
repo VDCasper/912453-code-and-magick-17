222771fc94173e8c363135d3592c5def902abb89
Dmitriy Kozlov <kdmitriys@gmail.com>
2019-06-11 15:53:59 +0300

    Настроил eslint и внес праки

--- js/stat.js
+++ js/stat.js
@@ -1,7 +1,7 @@
 'use strict';
 
-var renderStatistics = function(ctx, names, times) {
-  //Настройки окна статистики
+var renderStatistics = function (ctx, names, times) {
+  // Настройки окна статистики
   var sets = {
     fontFamily: 'PT Mono',
     defaultFontSize: 16,
@@ -24,45 +24,45 @@ var renderStatistics = function(ctx, names, times) {
       columnWidth: 40,
       columnsGap: 50,
       currentColor: 'rgba(255, 0, 0, 1)',
-      getOtherColor: function() {
+      getOtherColor: function () {
         return 'hsl(240,' + Math.random() * 100 + '%, 50%)';
       }
     }
   };
 
-  //Вывод модального окна с тенью
-  var drawModalWindow = function(ctx, x, y, width, height, bgColor) {
+  // Вывод модального окна с тенью
+  var drawModalWindow = function (context, x, y, width, height, bgColor) {
     var currentContextFillStyle = ctx.fillStyle;
 
-    ctx.fillStyle = sets.shadow.color;
-    ctx.fillRect(x + sets.shadow.shift, y + sets.shadow.shift, width, height);
-    ctx.fillStyle = bgColor ? bgColor : sets.defaultBgColor;
-    ctx.strokeRect(x, y ,width, height);
-    ctx.fillRect(x, y, width, height);
+    context.fillStyle = sets.shadow.color;
+    context.fillRect(x + sets.shadow.shift, y + sets.shadow.shift, width, height);
+    context.fillStyle = bgColor ? bgColor : sets.defaultBgColor;
+    context.strokeRect(x, y, width, height);
+    context.fillRect(x, y, width, height);
 
-    ctx.fillStyle = currentContextFillStyle;
+    context.fillStyle = currentContextFillStyle;
   };
 
   // Вывод многострочного текста в указанный контекст
   // Переносы строк по символу конца строки из текста
-  var printText = function(ctx, text, x, y, fontSize, color) {
-    var currentContextFillStyle = ctx.fillStyle; //сохраняем текущий стиль чтобы потом вернуть
-    ctx.font = fontSize + 'px ' + sets.fontFamily;
-    ctx.fillStyle = color ? color : sets.defaultColor;
+  var printText = function (context, text, x, y, fontSize, color) {
+    var currentContextFillStyle = context.fillStyle; // сохраняем текущий стиль чтобы потом вернуть
+    context.font = fontSize + 'px ' + sets.fontFamily;
+    context.fillStyle = color ? color : sets.defaultColor;
     var strings = (text + '').split('\n');
 
     // Выводим отдельные строки
     for (var i = 0; i < strings.length; i++) {
-      ctx.fillText(strings[i], x, y + fontSize * i);
+      context.fillText(strings[i], x, y + fontSize * i);
     }
 
-    ctx.fillStyle = currentContextFillStyle;
+    context.fillStyle = currentContextFillStyle;
   };
 
-  //Рисует гистограмму в заданном месте
-  var drawHistogram = function(ctx, x, y, names, times) {
-    //Сначала определяе максимальное время
-    var maxTime = function(numArray) {
+  // Рисует гистограмму в заданном месте
+  var drawHistogram = function (context, x, y, namesArray, timesArray) {
+    // Сначала определяе максимальное время
+    var maxTime = function (numArray) {
       var maxElement = 0;
       for (var i = 0; i < numArray.length; i++) {
         if (numArray[i] > maxElement) {
@@ -72,35 +72,41 @@ var renderStatistics = function(ctx, names, times) {
       return maxElement;
     };
 
-    //Находим коэффициент пропорциональности для высоты гистограммы с учетом надписей у каждой колонки
-    var mapRatio = maxTime(times) / (sets.hist.height - sets.defaultFontSize * 2);
+    // Находим коэффициент пропорциональности для высоты гистограммы с учетом надписей у каждой колонки
+    var mapRatio = maxTime(timesArray) / (sets.hist.height - sets.defaultFontSize * 2);
 
-    //Возвращает высоту колонки
-    var timeMap = function(time) {
+    // Возвращает высоту колонки
+    var timeMap = function (time) {
       return Math.floor(time / mapRatio);
     };
 
-    //Выводит колонку с надписями, для имени "Вы" назначает специальный цвет
-    var drawBar = function(count, name, time) {
+    // Выводит колонку с надписями, для имени "Вы" назначает специальный цвет
+    var drawBar = function (count, name, time) {
       var barX = x + count * (sets.hist.columnWidth + sets.hist.columnsGap); // начальная координата X
       var barY = y + sets.hist.height - sets.defaultFontSize * 2 - timeMap(time); // yачальная координата Y
 
-      printText(ctx, Math.floor(time), barX, barY, sets.defaultFontSize);
-      ctx.fillStyle = name === 'Вы' ? sets.hist.currentColor : sets.hist.getOtherColor();
-      ctx.fillRect(barX, barY + sets.defaultFontSize, sets.hist.columnWidth, timeMap(time));
-      printText(ctx, name, barX, barY + sets.defaultFontSize * 2 + timeMap(time), sets.defaultFontSize);
+      printText(context, Math.floor(time), barX, barY, sets.defaultFontSize);
+      context.fillStyle = name === 'Вы' ? sets.hist.currentColor : sets.hist.getOtherColor();
+      context.fillRect(barX, barY + sets.defaultFontSize, sets.hist.columnWidth, timeMap(time));
+      printText(
+          context,
+          name,
+          barX,
+          barY + sets.defaultFontSize * 2 + timeMap(time),
+          sets.defaultFontSize
+      );
     };
 
     // Выводим все колонки гистограммы в цикле
-    for (var i = 0; i < names.length; i++) {
-      drawBar(i, names[i], times[i]);
+    for (var i = 0; i < namesArray.length; i++) {
+      drawBar(i, namesArray[i], timesArray[i]);
     }
   };
 
-  //Поочередно выводим модальное окно, печатаем заголовок и рисуем гистограмму
+  // Поочередно выводим модальное окно, печатаем заголовок и рисуем гистограмму
   drawModalWindow(ctx, sets.window.x, sets.window.y, sets.window.width, sets.window.height);
   printText(ctx, sets.window.title, sets.window.x + 20, sets.window.y + 30, sets.defaultFontSize);
   drawHistogram(ctx, sets.window.x + 40, sets.window.y + sets.defaultFontSize + 70, names, times);
 };
 
-///EOF
+// /EOF

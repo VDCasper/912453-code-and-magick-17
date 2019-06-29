002c36e9eb79d2ccb4513da54653b6c05a9d6900
Dmitriy Kozlov <kdmitriys@gmail.com>
2019-06-30 02:27:36 +0300

    Обработка событий окна настройки персонажа

--- js/setup.js
+++ js/setup.js
@@ -4,8 +4,21 @@ var WISARDS_MAX = 4;
 var setupWindow = document.querySelector('.setup');
 var setupSimilar = document.querySelector('.setup-similar');
 var similarWizardTemplate = document
-.querySelector('#similar-wizard-template')
-.content.querySelector('.setup-similar-item');
+  .querySelector('#similar-wizard-template')
+  .content.querySelector('.setup-similar-item');
+
+var setupOpenButton = document.querySelector('.setup-open');
+var setupOpenIcon = document.querySelector('.setup-open-icon');
+var setupCloseButton = document.querySelector('.setup-close');
+var setupWizardForm = document.querySelector('.setup-wizard-form');
+var setupSubmitButton = document.querySelector('.setup-submit');
+
+// Изображение персонажа в окне настроек
+var wizardSetup = {
+  eyes: document.querySelector('.setup-wizard .wizard-eyes'),
+  coat: document.querySelector('.setup-wizard .wizard-coat'),
+  fireball: document.querySelector('.setup-fireball-wrap')
+};
 
 // Инициализируем набор возможных свойств персонажей
 var names = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
@@ -31,6 +44,8 @@ var coatColors = [
 ];
 
 var eyesColors = ['black', 'red', 'blue', 'yellow', 'green'];
+
+var fireballColors = ['#ee4830', '#30a8ee', '#5ce6c0', '#e848d5', '#e6e848'];
 
 // Возвращает случайный элемент массива
 function getRandomArrayElement(array) {
@@ -68,11 +83,88 @@ function addWizards(wizardsArray) {
   }
 }
 
+// Открывает окно настроек персонажа
+function openSetup() {
+  setupWindow.classList.remove('hidden');
+  setupSimilar.classList.remove('hidden');
+  setupCloseButton.addEventListener('click', onSetupWindowClose);
+  setupOpenButton.removeEventListener('click', onSetupWindowOpen);
+  setupOpenIcon.setAttribute('tabindex', '-1');
+  setupCloseButton.setAttribute('tabindex', '0');
+}
+
+// Закрывает окно настроек персонажа
+function closeSetup() {
+  setupWindow.classList.add('hidden');
+  setupSimilar.classList.add('hidden');
+  setupCloseButton.removeEventListener('click', onSetupWindowClose);
+  setupOpenButton.addEventListener('click', onSetupWindowOpen);
+  setupOpenIcon.setAttribute('tabindex', '0');
+  setupCloseButton.setAttribute('tabindex', '-1');
+}
+
+// Обработчики событий
+
+function onSetupWindowOpen(evt) {
+  evt.preventDefault();
+  openSetup();
+}
+
+function onSetupWindowClose(evt) {
+  evt.preventDefault();
+  closeSetup();
+}
+
+function onKeyDown(evt) {
+  switch (evt.keyCode) {
+    case 13:
+      if (setupOpenIcon === document.activeElement) {
+        evt.preventDefault();
+        openSetup();
+      }
+      if (setupCloseButton === document.activeElement) {
+        evt.preventDefault();
+        closeSetup();
+      }
+      break;
+    case 27:
+      if (
+        !setupWizardForm.contains(document.activeElement) &&
+        !setupWindow.classList.contains('hidden')
+      ) {
+        evt.preventDefault();
+        closeSetup();
+      }
+      break;
+  }
+}
+
+function onEyesClick(evt) {
+  evt.target.style.fill = getRandomArrayElement(eyesColors);
+}
+
+function onCoatClick(evt) {
+  evt.target.style.fill = getRandomArrayElement(coatColors);
+}
+
+function onFireballClick(evt) {
+  evt.target.style.backgroundColor = getRandomArrayElement(fireballColors);
+}
+
 // Выводим случайных волшебников на страницу
 addWizards(generateRandomWizards());
 
-// Показываем окно настроек персонажа
-setupWindow.classList.remove('hidden');
-setupSimilar.classList.remove('hidden');
+// Задаем недостающие атрибуты формы настройки персонажа
+setupOpenIcon.setAttribute('tabindex', '0');
+setupWizardForm.setAttribute('method', 'POST');
+setupWizardForm.setAttribute('action', 'https://js.dump.academy/code-and-magick');
+setupSubmitButton.setAttribute('type', 'submit');
+
+// Добавляем обработчики событий
+setupOpenButton.addEventListener('click', onSetupWindowOpen);
+window.addEventListener('keydown', onKeyDown);
+wizardSetup.eyes.addEventListener('click', onEyesClick);
+wizardSetup.coat.addEventListener('click', onCoatClick);
+wizardSetup.fireball.addEventListener('click', onFireballClick);
 
 // EOF

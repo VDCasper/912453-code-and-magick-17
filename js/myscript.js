3d8a6a5f1ecb429bcf989b2562611548dbbf5cb3
Vdcasper <Vdcasper@gmail.com>
2019-07-19 13:06:33 +0300

    first task add js file

---
+++ js/myscript.js
@@ -0,0 +1,10 @@
+var fireballSize = 22;
+let getFireballSpeed = (left)  => left ? 5:2;
+var wizardSpeed  = 3;
+var wizardWidth = 70;
+let getWizardHeight = (wizardWidth) => 1.337 * wizardWidth;
+var getWizardX = function(width) {
+    return width / 2};
+
+var getWizardY = function(height) {
+    return (height * 2 / 4)};
